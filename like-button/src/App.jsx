3fd7504a12870023e8814a1ef4f30ff49ab5c961
Daigo Hirbo <daigoab2017@gmail.com>
2023-07-06 17:17:16 +0300

    initial commit

---
+++ like-button/src/App.jsx
@@ -0,0 +1,14 @@
+import LikeButton from './components/LikeButton'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <LikeButton/>
+    </>
+  )
+}
+
+export default App
---
+++ like-button/src/components/LikeButton.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+const LikeButton = () => {
+    const [count, setCount] = useState(0);
+  
+    const handleLikeClick = () => {
+      setCount(count + 1);
+    };
+  
+    const handleUnlikeClick = () => {
+      setCount(count - 1);
+    };
+
+    return (
+        <div>
+          <button onClick={handleLikeClick}>
+            <span role="img" aria-label="Heart">
+               👍 
+            </span>{" "}
+            Like
+          </button>
+          <span> {count} Likes</span>
+          <button onClick={handleUnlikeClick}>
+            <span role="img" aria-label="Heart">
+              👎
+            </span>{" "}
+            Unlike
+          </button>
+        </div>
+      );
+    };
+
+export default LikeButton
